9b81122c94a5b92cc6d8049f8f661ebd75ca806c
dusankaK <karacdusanka11@gmail.com>
2020-04-03 15:03:15 +0200

    #DKAH-13 added home controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //
+    public function index() 
+    {
+        $name = 'Dusanka';
+        $age = 26;
+        return view('welcome', compact('name', 'age'));
+
+    }
+
+    public function greeting()
+    {
+        return view('simple', ['first_name' => 'Dušanka']);
+
+    }
+
+    public function age($age) 
+    {
+        return "You are {$age} years old.";
+    }
+
+    //crete, store, show, edit, update, destroy
+    public function create()
+    {
+        return 'Welcome to dashboard';
+    }
+
+    public function store() 
+    {
+        //Some code here
+        return 'User stored';
+    }
+
+    public function show($id) 
+    {
+
+    }
+
+    public function edit($id) {
+        return 'Make some changes';
+    }
+
+    public function update($id) {
+        return 'Make some changes';
+    }
+    
+    public function destroy($id) {
+        return 'Delete this user';
+    }
+}
